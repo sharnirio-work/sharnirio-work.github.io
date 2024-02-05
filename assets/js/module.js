c2f81fc6fcc146a19a0c8da14f1f3bc16830ba23
sharnirio-work <vyacheslav@heyreliable.com>
2024-02-05 17:03:09 +0200

    many fix on old modules

--- assets/js/module.js
+++ assets/js/module.js
@@ -29362,50 +29362,6 @@ for(d=0;d<a.options.fonts.length;d++)c+="<option>"+a.options.fonts[d]+"</option>
 (c+='<li class="zoom-reset"></li>');a.box.html(c+"</ul>");a.buttons={zoom_in:a.box.find(".zoom-in"),zoom_out:a.box.find(".zoom-out"),font:a.box.find(".zoom-font-family select"),left:a.box.find(".zoom-left"),center:a.box.find(".zoom-center"),right:a.box.find(".zoom-right"),bold:a.box.find(".zoom-bold"),color_box:a.box.find(".zoom-color"),color_preview:a.box.find(".color-preview"),colors_list:a.box.find(".zoom-colors-list"),reset:a.box.find(".zoom-reset")};return jQuery.extend(this,a)};e.prototype.restore=
 function(a){0<this.buttons.color_preview.length&&this.buttons.color_preview.css("background-color",a.color);this.buttons.font.val(a.font_family)};f.fn.zoom.panel=function(a,c,d){a=new e(a,c,d);h(a);return a}})(jQuery);
 
-/* module image-map */
-// function on
-jQuery(document).ready(function() {
-	imgMapInit();
-});
-
-// more info https://github.com/stowball/jQuery-rwdImageMaps
-// function init
-function imgMapInit() {
-	$('img[usemap]').rwdImageMaps();
-	$('area').on('click', function(e) {
-		e.preventDefault();
-		var target = $(this).attr('href');
-		var imagemapItems = $('.imagemap__item');
-		imagemapItems.hide();
-		$(target).show();
-		$(target + '> h4').addClass('is-open');
-	});
-}
-
-/*
- * rwdImageMaps jQuery plugin v1.5
- *
- * Allows image maps to be used in a responsive design by recalculating the area coordinates to match the actual image size on load and window.resize
- *
- * Copyright (c) 2013 Matt Stow
- * https://github.com/stowball/jQuery-rwdImageMaps
- * http://mattstow.com
- * Licensed under the MIT license
- */
-;
-(function(a) { a.fn.rwdImageMaps = function() { var c = this; var b = function() { c.each(function() { if (typeof(a(this).attr("usemap")) == "undefined") { return } var e = this,
-					d = a(e);
-				a("<img />").load(function() { var g = "width",
-						m = "height",
-						n = d.attr(g),
-						j = d.attr(m); if (!n || !j) { var o = new Image();
-						o.src = d.attr("src"); if (!n) { n = o.width } if (!j) { j = o.height } } var f = d.width() / 100,
-						k = d.height() / 100,
-						i = d.attr("usemap").replace("#", ""),
-						l = "coords";
-					a('map[name="' + i + '"]').find("area").each(function() { var r = a(this); if (!r.data(l)) { r.data(l, r.attr(l)) } var q = r.data(l).split(","),
-							p = new Array(q.length); for (var h = 0; h < p.length; ++h) { if (h % 2 === 0) { p[h] = parseInt(((q[h] / n) * 100) * f) } else { p[h] = parseInt(((q[h] / j) * 100) * k) } } r.attr(l, p.toString()) }) }).attr("src", d.attr("src")) }) };
-		a(window).resize(b).trigger("resize"); return this } })(jQuery);
 
 /* module data-tables */
 // function on
